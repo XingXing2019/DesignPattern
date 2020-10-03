a2c6b460b42f22f75b84b133fc3bc118640d835c
Xing <x.xing.unsw@gmail.com>
2020-10-03 21:52:30 +1000

    init

---
+++ TemplateMethod/AnimalToFridge/AnimalToFridge/AnimalToFridge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AnimalToFridge
+{
+    public class AnimalToFridge
+    {
+        public void Do(Action beforePutInFridge, Action putInFridge)
+        {
+            OpenFridge();
+            beforePutInFridge?.Invoke();
+            putInFridge?.Invoke();
+            CloseFridge();
+        }
+
+        private void OpenFridge()
+        {
+            Console.WriteLine("Open Fridge");
+        }
+
+        private void CloseFridge()
+        {
+            Console.WriteLine("Close Fridge");
+        }
+    }
+}
---
+++ TemplateMethod/AnimalToFridge/AnimalToFridge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AnimalToFridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elephant = new AnimalToFridge();
+            elephant.Do(null, () => Console.WriteLine("Put elephant into fridge"));
+            Console.WriteLine("----------------------");
+            var giraffe = new AnimalToFridge();
+            giraffe.Do(() => Console.WriteLine("Take elephant out"), () => Console.WriteLine("Put giraffe into fridge"));
+        }
+    }
+}
